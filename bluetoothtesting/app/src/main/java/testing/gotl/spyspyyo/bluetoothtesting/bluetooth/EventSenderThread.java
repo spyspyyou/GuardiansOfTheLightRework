677cb235da3dc201eaf80d3ebed3cb9bae100dbf
spyspyyou <sandro.rueegge@hispeed.ch>
2017-01-17 17:38:31 +0100

    forgot last commit

--- bluetoothtesting/app/src/main/java/testing/gotl/spyspyyo/bluetoothtesting/bluetooth/EventSenderThread.java
+++ bluetoothtesting/app/src/main/java/testing/gotl/spyspyyo/bluetoothtesting/bluetooth/EventSenderThread.java
@@ -7,24 +7,59 @@ import java.io.UnsupportedEncodingException;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
-//this thread also regularly sends the HandshakeEvents
+import static testing.gotl.spyspyyo.bluetoothtesting.teststuff.TODS.TEXT_ENCODING;
+
 /*package*/ class EventSenderThread extends Thread {
 
+    private static final int
+            /**
+             * Amount of time in millis to wait until cancelling the polling of the next event
+             */
+            MAXIMUM_WAIT_TIME = 1000;
 
+    private static final LinkedBlockingQueue<Event>
+            events = new LinkedBlockingQueue<>();
+
+    private static boolean
+            activeThreadExisting = false;
+
+    /*package*/ EventSenderThread(){
         start();
     }
 
     @Override
     public void run() {
+        if (activeThreadExisting) return;
+        activeThreadExisting = true;
+        Log.i("ESThread", "started");
+
+        Event event = null;
+        while (ConnectionManager.hasConnections()) {
             try {
+                event = events.poll(MAXIMUM_WAIT_TIME, TimeUnit.MILLISECONDS);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+
+            if (event != null) {
+
+                for (String address:event.getReceptors()) {
+                    try {
+                        ConnectionManager.send(event.getReceptors(), event.toString().getBytes(TEXT_ENCODING));
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
         }
+
+        activeThreadExisting = false;
     }
 
+    /*package*/ static void send(Event event) {
         if (!events.offer(event)) {
-            event.onEventSendFailure(event.getReceptors());
+            Log.w("ESThread", "Couldn't add Event.");
         }
     }
 }
+
