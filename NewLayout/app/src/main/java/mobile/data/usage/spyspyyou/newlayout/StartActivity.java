1317bd958dd3c9431299a6a78a5e5d22c7a5a249
spyspyyou <sandro.rueegge@hispeed.ch>
2017-01-28 18:39:44 +0100

    new layout nice coming

--- NewLayout/app/src/main/java/mobile/data/usage/spyspyyou/newlayout/StartActivity.java
+++ NewLayout/app/src/main/java/mobile/data/usage/spyspyyou/newlayout/StartActivity.java
@@ -1,29 +1,39 @@
 package mobile.data.usage.spyspyyou.newlayout;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class StartActivity extends FragmentActivity {
+public class StartActivity extends AppCompatActivity {
 
     DemoCollectionPagerAdapter mDemoCollectionPagerAdapter;
     ViewPager mViewPager;
+    ActionBarDrawerToggle mDrawerToggle;
+
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        final FloatingActionButton fabCreate = (FloatingActionButton) findViewById(R.id.fab_create);
+        fabCreate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //todo:start  the lobby
@@ -31,6 +41,50 @@ public class StartActivity extends FragmentActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        final FloatingActionButton fabWorlds = (FloatingActionButton) findViewById(R.id.fab_worlds);
+        fabWorlds.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //todo:start  the lobby
+                Snackbar.make(view, "Create a new World", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar == null)return;
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setElevation(2);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        final TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.hello_world, R.string.hello_world) {
+
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                super.onDrawerSlide(drawerView, slideOffset);
+                fabCreate.hide();
+                fabWorlds.hide();
+                if (slideOffset <= 0.2){
+                    if (tabLayout.getSelectedTabPosition() == 0)fabCreate.show();
+                    if (tabLayout.getSelectedTabPosition() == 2)fabWorlds.show();
+                }
+            }
+
+            public void onDrawerClosed(View view) {
+                supportInvalidateOptionsMenu();
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                supportInvalidateOptionsMenu();
+            }
+        };
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        drawerLayout.addDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
 
         // ViewPager and its adapters use support library
         // fragments, so use getSupportFragmentManager.
@@ -45,8 +99,11 @@ public class StartActivity extends FragmentActivity {
 
             @Override
             public void onPageSelected(int position) {
-                if (position == 0)fab.show();
-                else fab.hide();
+                if (position == 0)fabCreate.show();
+                else fabCreate.hide();
+
+                if (position == 2)fabWorlds.show();
+                else fabWorlds.hide();
             }
 
             @Override
@@ -54,8 +111,23 @@ public class StartActivity extends FragmentActivity {
 
             }
         });
-        final TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+
         tabLayout.setupWithViewPager(mViewPager);
+    }
+
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState)
+    {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig)
+    {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 
 
